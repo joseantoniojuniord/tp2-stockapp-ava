f6009615b4800edef2264f9a66d7511eb4d616fc
joseantoniojuniord <junior_antonio_alves@protonmail.com>
2024-11-20 16:40:16 -0300

    Criação de Exceções Personalizadas para Autenticação

---
+++ StockApp.Domain/Exceptions/AuthenticationException.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace StockApp.Domain.Exceptions
+{
+    public class AuthenticationException : Exception
+    {
+        public HttpStatusCode StatusCode {  get; }
+        public string ErrorCode {  get; }
+
+        public AuthenticationException(string message, string errorCode = "Erro de autenticação", HttpStatusCode statusCode = HttpStatusCode.Unauthorized)
+            : base(message) 
+        {
+            StatusCode = statusCode;
+            ErrorCode = errorCode;
+        }
+
+        public AuthenticationException(string message, HttpStatusCode statusCode = HttpStatusCode.Unauthorized) 
+            : this(message, "Erro de autenticação", statusCode) { }
+    }
+}
